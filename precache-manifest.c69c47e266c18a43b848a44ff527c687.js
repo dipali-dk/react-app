b1f9107897e5c60c28463aa527a2cb224e6e49ef
dipali-dk <dipali19kataruaka@gmail.com>
2020-03-03 22:24:45 +0530

    Initial Commit

---
+++ precache-manifest.c69c47e266c18a43b848a44ff527c687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7961af4783614aa01273011f18c669bc",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "bcbdab07b8a37efbc50e",
+    "url": "/react-tutorial/static/css/main.b6d41ff7.chunk.css"
+  },
+  {
+    "revision": "6ce31b2486539ef696df",
+    "url": "/react-tutorial/static/js/2.f9540ce4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tutorial/static/js/2.f9540ce4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbdab07b8a37efbc50e",
+    "url": "/react-tutorial/static/js/main.3f491138.chunk.js"
+  },
+  {
+    "revision": "56b884f0bc5e252c6869",
+    "url": "/react-tutorial/static/js/runtime-main.f3423374.js"
+  }
+]);
